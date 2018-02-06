515a8e430fdfac0f55dd27950e173a7576705c5c
Xeng Xiong <waterfish_21@yahoo.com>
2018-02-06 20:38:35 +0000

    2nd commit, chapter 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @number_of_recipes = 5
+    @number_of_recipes = Recipe.count
+    @title = "Home"
   end
   def about
+    @title = "About"
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :recipes
   get '/about', to: 'home#about', as: 'about'
   root to: 'home#index'
 
